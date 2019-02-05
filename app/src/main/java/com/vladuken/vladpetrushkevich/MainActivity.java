db1d88cbf220cd2d6d673f055114f0daa5c29b56
Vladuken <vladpetrushkevich1999@gmail.com>
2019-02-05 03:33:56 +0300

    Add my card app

---
+++ app/src/main/java/com/vladuken/vladpetrushkevich/MainActivity.java
@@ -0,0 +1,38 @@
+package com.vladuken.vladpetrushkevich;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private CardView mCardView;
+    private TextView mFullName;
+    private TextView mGitHubLink;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mCardView = findViewById(R.id.photo_card_view);
+
+        mFullName = findViewById(R.id.full_name_text_view);
+
+        mGitHubLink = findViewById(R.id.github_link_text_view);
+        mGitHubLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String link = getResources().getString(R.string.github_link);
+                Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(link));
+                startActivity(i);
+            }
+        });
+    }
+
+
+}
