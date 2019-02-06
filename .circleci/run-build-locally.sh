12dcbea76653f8d7fabfbe88172bfc4903f59ee9
Vladuken <vladpetrushkevich1999@gmail.com>
2019-02-06 04:32:03 +0300

    Remove findbug

--- .circleci/run-build-locally.sh
+++ .circleci/run-build-locally.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 curl --user 7fc758b99c7d6f1f9ddf2260927443bd61d9aa77: \
     --request POST \
-    --form revision=2b426454a648bcf6f8d8d4c4b14528e1eb9c392d\
+    --form revision=bd9883ba8c958f5a3e52808ca7daa83513e16512\
     --form config=@config.yml \
     --form notify=false \
         https://circleci.com/api/v1.1/project/github/Vladuken/KMP-HomeWork1/tree/feature
