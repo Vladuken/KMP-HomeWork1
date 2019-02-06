bd9883ba8c958f5a3e52808ca7daa83513e16512
Vladuken <vladpetrushkevich1999@gmail.com>
2019-02-06 04:20:06 +0300

    Circle ci + Appcenter config

--- .circleci/run-build-locally.sh
+++ .circleci/run-build-locally.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 curl --user 7fc758b99c7d6f1f9ddf2260927443bd61d9aa77: \
     --request POST \
-    --form revision=80a489719309df07fa4ce9f69c861aa29005c641\
+    --form revision=2b426454a648bcf6f8d8d4c4b14528e1eb9c392d\
     --form config=@config.yml \
     --form notify=false \
         https://circleci.com/api/v1.1/project/github/Vladuken/KMP-HomeWork1/tree/feature
--- app/src/main/java/com/vladuken/vladpetrushkevich/MainActivity.java
+++ app/src/main/java/com/vladuken/vladpetrushkevich/MainActivity.java
@@ -9,29 +9,22 @@ import android.widget.TextView;
 import com.crashlytics.android.Crashlytics;
 import io.fabric.sdk.android.Fabric;
 
-
 import com.microsoft.appcenter.AppCenter;
 import com.microsoft.appcenter.analytics.Analytics;
 import com.microsoft.appcenter.crashes.Crashes;
+import com.microsoft.appcenter.distribute.Distribute;
 
 public class MainActivity extends AppCompatActivity {
-
-    //private CardView mCardView;
-    //private TextView mFullName;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        AppCenter.start(getApplication(), "77770b97-7fc5-4620-b874-95c26bb3e37c", Analytics.class, Crashes.class);
 
         Fabric.with(this, new Crashlytics());
+        AppCenter.start(getApplication(), "77770b97-7fc5-4620-b874-95c26bb3e37c", Analytics.class, Crashes.class, Distribute.class);
         setContentView(R.layout.activity_main);
 
-        //mCardView = findViewById(R.id.photo_card_view);
-
-        //mFullName = findViewById(R.id.full_name_text_view);
         TextView gitHubLink;
         gitHubLink = findViewById(R.id.github_link_text_view);
         gitHubLink.setOnClickListener(new View.OnClickListener() {
