a63b02eb99e5363bf2321e651a1e6860637208b3
Vladuken <vladpetrushkevich1999@gmail.com>
2019-02-07 18:24:21 +0300

    Add empty ViewPager

---
+++ app/src/main/java/com/vladuken/vladpetrushkevich/WelcomeActivity.java
@@ -0,0 +1,107 @@
+package com.vladuken.vladpetrushkevich;
+
+import android.content.Context;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    private ViewPager mViewPager;
+    private WelcomeViewPagerAdapter mViewPagerAdapter;
+    private LinearLayout mDotsLayout;
+    private TextView[] mDots;
+    private int[] mLayouts;
+    private Button mBtnSkip, mBtnNext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Making notification bar transparent
+        if (Build.VERSION.SDK_INT >= 21) {
+            getWindow()
+                    .getDecorView()
+                    .setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        }
+
+        setContentView(R.layout.activity_welcome);
+
+        mViewPager = findViewById(R.id.view_pager);
+        mBtnSkip = (Button) findViewById(R.id.btn_skip);
+        mBtnNext = (Button) findViewById(R.id.btn_next);
+
+
+        mLayouts = new int[]{
+                R.layout.welcome_slide1,
+                R.layout.welcome_slide2,
+                R.layout.welcome_slide3,
+                R.layout.welcome_slide4};
+
+        mViewPagerAdapter = new WelcomeViewPagerAdapter();
+        mViewPager.setAdapter(mViewPagerAdapter);
+//        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+//            @Override
+//            public void onPageScrolled(int i, float v, int i1) {
+//
+//            }
+//
+//            @Override
+//            public void onPageSelected(int i) {
+//
+//            }
+//
+//            @Override
+//            public void onPageScrollStateChanged(int i) {
+//
+//            }
+//        });
+    }
+
+
+
+    public class WelcomeViewPagerAdapter extends PagerAdapter {
+
+        private LayoutInflater layoutInflater;
+
+        public WelcomeViewPagerAdapter() {
+        }
+
+        @NonNull
+        @Override
+        public Object instantiateItem(@NonNull ViewGroup container, int position) {
+            layoutInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+            View view = layoutInflater.inflate(mLayouts[position], container, false);
+            container.addView(view);
+
+            return view;
+        }
+
+        @Override
+        public int getCount() {
+            return mLayouts.length;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object o) {
+            return view == o;
+        }
+
+        @Override
+        public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+            View view = (View) object;
+            container.removeView(view);
+        }
+    }
+
+}
