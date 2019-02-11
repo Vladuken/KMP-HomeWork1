a36b6a4007f11afa173a76243cc0a9dcfe7d8807
Vladuken <vladpetrushkevich1999@gmail.com>
2019-02-11 23:36:30 +0300

    [launcher-activity] Add Apps to activity + launch onClick

--- app/src/main/java/com/vladuken/vladpetrushkevich/LauncherActivity.java
+++ app/src/main/java/com/vladuken/vladpetrushkevich/LauncherActivity.java
@@ -2,11 +2,12 @@ package com.vladuken.vladpetrushkevich;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.content.res.Configuration;
-import android.graphics.Color;
-import android.graphics.ColorSpace;
-import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
@@ -14,19 +15,22 @@ import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.SparseIntArray;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.vladuken.vladpetrushkevich.settings.SettingsActivity;
 
-import java.util.Random;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class LauncherActivity extends AppCompatActivity {
 
@@ -35,10 +39,8 @@ public class LauncherActivity extends AppCompatActivity {
     //TODO change preference to string xml
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        SharedPreferences sharedPref =
-                getSharedPreferences(Constants.SharedPreferences.APP_PREFERENCES,0);
+        SharedPreferences sharedPref = getSharedPreferences(Constants.SharedPreferences.APP_PREFERENCES,0);
         boolean theme = sharedPref.getBoolean(Constants.SharedPreferences.THEME, false);
-
         if(!theme){
             setTheme(R.style.AppTheme);
         }else{
@@ -46,16 +48,6 @@ public class LauncherActivity extends AppCompatActivity {
         }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nav_drawer);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view,"TODO",Snackbar.LENGTH_SHORT)
-                        .setAction("Action",null)
-                        .show();
-            }
-        });
 
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this::onNavigationItemSelected);
@@ -91,26 +83,48 @@ public class LauncherActivity extends AppCompatActivity {
             mRecyclerView.setLayoutManager(new GridLayoutManager(this, landscapeSpanCount));
         }
 
-        LauncherAdapter mAdapter;
-        mAdapter = new LauncherAdapter();
-        mRecyclerView.setAdapter(mAdapter);
+        setupAdapter();
         int offset = getResources().getDimensionPixelOffset(R.dimen.offset);
         mRecyclerView.addItemDecoration(new LauncherItemDecoration(offset));
 
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view,"TODO",Snackbar.LENGTH_SHORT)
+                        .setAction("Action",null)
+                        .show();
+            }
+        });
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
+    private void setupAdapter() {
+        Intent startupIntent = new Intent(Intent.ACTION_MAIN);
+        startupIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+
+        PackageManager pm = getPackageManager();
+        List<ResolveInfo> activities = pm.queryIntentActivities(startupIntent, 0);
+        Collections.sort(activities, new Comparator<ResolveInfo>() {
+            @Override
+            public int compare(ResolveInfo a, ResolveInfo b) {
+                PackageManager pm = getPackageManager();
+                return String.CASE_INSENSITIVE_ORDER.compare(
+                        a.loadLabel(pm).toString(),
+                        b.loadLabel(pm).toString());
+            }
+        });
+        mRecyclerView.setAdapter(new LauncherAdapter(activities));
     }
+
 
     public class LauncherAdapter extends RecyclerView.Adapter<LauncherViewHolder>{
+        private final List<ResolveInfo> mInstalledAppInfo;
+        private final Map<ResolveInfo,Drawable> mIcons;
 
-        private final Random mRandom = new Random();
-        private final SparseIntArray mColorMap = new SparseIntArray();
-
-        public LauncherAdapter() {
-            //TODO
+        public LauncherAdapter(List<ResolveInfo> installedAppsInfo) {
+            mInstalledAppInfo =  installedAppsInfo;
+            mIcons = new HashMap<>();
         }
 
         @NonNull
@@ -119,54 +133,61 @@ public class LauncherActivity extends AppCompatActivity {
             View view = LayoutInflater.from(parent.getContext())
                     .inflate(R.layout.item_icon_view,parent,false);
 
-            view.setOnLongClickListener(new View.OnLongClickListener() {
-                @Override
-                public boolean onLongClick(View v) {
-                    int color = Color.TRANSPARENT;
-                    Drawable background = v.getBackground();
-                    if (background instanceof ColorDrawable)
-                        color = ((ColorDrawable) background).getColor();
-                    String textColor =  String.format("#%06X", (0xFFFFFF & color));
-                    Snackbar.make(v,textColor,Snackbar.LENGTH_SHORT)
-                            .show();
-                    return true;
-                }
-            });
             return new LauncherViewHolder(view);
         }
 
         @Override
         public void onBindViewHolder(@NonNull LauncherViewHolder viewHolder, int position) {
-            viewHolder.itemView.setBackgroundColor(getColor(position));
+            ResolveInfo resolveInfo = mInstalledAppInfo.get(position);
+            PackageManager pm = getPackageManager();
+
+            if(mIcons.get(resolveInfo) == null){
+                mIcons.put(resolveInfo,mInstalledAppInfo.get(position).loadIcon(pm));
+            }
+
+            viewHolder.bind(resolveInfo,mIcons.get(resolveInfo));
         }
 
         @Override
         public int getItemCount() {
-            return 100;
-        }
-
-        private int getColor(int position){
-            Integer color = mColorMap.get(position);
-            if(color == 0)
-            {
-                color = Color.rgb(mRandom.nextInt(256),
-                        mRandom.nextInt(256),
-                        mRandom.nextInt(256)
-                );
-
-                mColorMap.put(position,color);
-            }
-
-            return color;
+            return mInstalledAppInfo.size();
         }
     }
 
     public class LauncherViewHolder extends RecyclerView.ViewHolder{
 
-        View mView;
+        private ResolveInfo mResolveInfo;
+
+        private final ImageView mAppIcon;
+        private final TextView mAppTitle;
+
         public LauncherViewHolder(@NonNull View itemView) {
             super(itemView);
-            mView = itemView;
+            mAppIcon = itemView.findViewById(R.id.app_icon_with_title);
+            mAppTitle = itemView.findViewById(R.id.app_title);
+            itemView.setOnClickListener(this::onClick);
+
+        }
+
+        public void bind(ResolveInfo resolveInfo,Drawable icon) {
+
+            mResolveInfo = resolveInfo;
+            PackageManager pm = getPackageManager();
+            String appName = mResolveInfo.loadLabel(pm).toString();
+
+            mAppIcon.setImageDrawable(icon);
+            mAppTitle.setText(appName);
+        }
+
+        public void onClick(View v) {
+            ActivityInfo activityInfo = mResolveInfo.activityInfo;
+
+            Intent i = new Intent(Intent.ACTION_MAIN)
+                    .setClassName(activityInfo.applicationInfo.packageName,
+                            activityInfo.name)
+                    .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+            startActivity(i);
         }
     }
 
